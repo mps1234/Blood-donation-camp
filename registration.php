6aeff3e80b2002ea7901366fa3e06bdf4a5b58fc
Saurabh0707 <imtemptedguy@gmail.com>
2017-02-14 11:39:57 +0530

    Integrated code alongwith design

---
+++ registration.php
@@ -0,0 +1,40 @@
+<?php
+include('includes/dbconnect.php');
+
+		//Variables declaration
+		$student_no = $name = $gender = $year = $blood_group = "";
+		$email = $contact_no = $hosteller = "" ;
+
+
+
+	if(isset($_POST['submit']))
+		{
+            $student_no = mysqli_real_escape_string($con,$_POST['studentno']);
+			$name = mysqli_real_escape_string($con,$_POST['name']);
+			$gender = mysqli_real_escape_string($con,$_POST['gender']);
+			$year = mysqli_real_escape_string($con,$_POST['year']);
+			$blood_group = mysqli_real_escape_string($con,$_POST['bloodgroup']);
+			$email = mysqli_real_escape_string($con,$_POST['email']);
+			$contact_no = mysqli_real_escape_string($con,$_POST['contactno']);
+			$hosteller = mysqli_real_escape_string($con,$_POST['hosteller']);
+
+		}
+
+		//Query to insert into database
+		$sql = "INSERT INTO registration_data(registration_id,student_no,name,gender,year,blood_group,email,contact_no,hosteller)
+			
+				VALUES(NULL,'$student_no','$name','$gender','$year','$blood_group','$email','$contact_no','$hosteller')";
+
+		//Execute the query
+		$result = mysqli_query($con,$sql);
+
+		if($result)
+			{	
+			echo "Registered Successfully";
+				}
+		else
+			{
+			echo "Failure to register";
+				}
+
+				?>
